8ce3737e3a2363afa3c191ef42920a121315ad19
asadbek-sotiboldiyev <ayebkovichas0209@gmail.com>
2026-08-22 14:41:43 +0500

    run edited

--- run.bat
+++ run.bat
@@ -1,18 +1,18 @@
 @echo off
-color 0F
 
+color 0F
 title Run FastAPI
 
 if not exist "myenv\Scripts\python.exe" (
     echo Virtual environment not found.
     echo Run install.bat first.
-    echo Trying with global python env.
-uvicorn main_app.server:app --reload
-)
-else (
-    echo Activating virtual environment...
-    call myenv\Scripts\activate.bat
-uvicorn main_app.server:app --reload
+    echo Trying with global Python...
+
+    python -m uvicorn main_app.server:app --reload
+) else (
+    echo Starting FastAPI from virtual environment...
+
+    myenv\Scripts\python.exe -m uvicorn main_app.server:app --reload
 )
 
 pause
